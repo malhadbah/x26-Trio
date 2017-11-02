0d8f81c9ee1f6ef0bdaec28559cb335f1b89a8f2
malhadbah <malhadbah@mail.csuchico.edu>
2017-11-01 21:31:40 -0700

    last version

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 void color(int & ,int & ,int &);
-void swap(int &, int &, int &);
 
 int main()
 {
@@ -11,9 +10,6 @@ int main()
         cout<<"Enter Red, Green, and Blue values: ";
         cin>>red>>green>>blue;
         //...END OF "DO NOT CHANGE" AREA
-
-        cout<<"The order of the three colors are: "<<endl;
-
         color(red,green,blue);
 
         //DO NOT CHANGE WITHIN THIS AREA...
@@ -22,22 +18,46 @@ int main()
         return 0;
         //...END OF "DO NOT CHANGE" AREA
 }
+
 void color(int& red,int& green,int& blue)
+
 {
-        if(red>green) {
-                int temp = red;
-                red = green;
-                green = temp;
-        }
-        if(red>blue) {
-                int temp = red;
-                red=blue;
-                blue= temp;
-        }
-        if(green>blue){
-                int temp = green;
-                green= blue;
-                blue = temp;
-        }
-        return;
-}
+
+        if(red>green && green>blue){//321
+        red=red;
+        green=green;
+        blue=blue;}
+                
+       
+        else if(red>blue && green<blue) {//312
+         int temp=green;
+          red=red;
+          green=blue;
+          blue=temp;
+          }
+          else if(red<green && red>blue){ //231
+          int temp=red;
+          red=green;
+          blue=blue;
+          green=temp;//temp=2 in this case
+          }
+          else if(red>green && red<blue){//213
+          int temp=red;
+          red=blue;
+          blue=green;
+          green=temp;
+          }
+          else if(red<green && green<blue){//123
+          int temp=red;
+          red=blue;
+          green=green;
+          blue=temp;
+          }
+          else if(red<blue && blue<green){//132
+          int temp=red;
+          red=green;
+          green=blue;
+          blue=temp;
+          }
+          }
+
